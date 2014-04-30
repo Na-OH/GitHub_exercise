98a5f0449bd9f6e78ad85a7d25fde2a3d97f7a97
Na-OH <gedojudah@gmail.com>
2014-04-30 16:03:12 +0900

    Add:mysqli_set_charset() function()

--- db_test.php
+++ db_test.php
@@ -1,8 +1,9 @@
 <?php 
-  $link = mysqli_connect('localhost', 'root', "");
+  $link = mysqli_connect('localhost', 'root', '');
   if (!$link) {
-    die('接続失敗！'.mysqli_error());
+    die('接続失敗！<br>'.mysqli_error());
   }else{
     echo '接続成功！';
+    mysqli_set_charset($link, "utf8");
   }
 ?>
